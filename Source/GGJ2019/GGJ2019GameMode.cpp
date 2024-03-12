bb23b8ff265d08f0f4948f30e232d8bba3c2c89e
JosteinE <Josteineri@hotmail.com>
2024-03-12 23:25:38 +0100

    Crash fixes, attenuation & exportable

--- Source/GGJ2019/GGJ2019GameMode.cpp
+++ Source/GGJ2019/GGJ2019GameMode.cpp
@@ -7,7 +7,7 @@
 AGGJ2019GameMode::AGGJ2019GameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/TheCharacter/ThirdPersonCharacter.ThirdPersonCharacter_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
